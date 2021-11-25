c52c303556d28fa128a6d15be143389ff4e8c3c3
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-11-25 21:50:27 +0300

    add database configurations

--- gallery/settings.py
+++ gallery/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'synthia',
+    'PASSWORD':'123',
     }
 }
 
