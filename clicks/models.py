f45d397a813e6e7cf8bdcce44e835abc2f5c8a78
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-11-27 21:45:34 +0300

    add methods to image model

--- clicks/models.py
+++ clicks/models.py
@@ -5,39 +5,50 @@ from django.utils import timezone
 from uuid import uuid4
 from django.conf import settings
 from django.urls import reverse
-import json
-import os
+
 
 # Create your models here.
 class Category(models.Model):
     title = models.CharField(null=True,blank=True,max_length=200)
     uniqueId =models.CharField(null=True,blank=True,max_length=200)
-    slug = models.SlugField(max_length=500,unique=True,blank=True,null=True)
     date_created = models.DateTimeField(blank=True,null=True)
     last_updated = models.DateTimeField(blank=True,null=True)
+
+    @classmethod  
+    def search_by_category(cls,search_term):
+        category = cls.objects.filter(title__icontains=search_term)
+        return category 
 
     def __str__(self):
         return '{} {}'.format(self.title,self.uniqueId)
 
     def get_absolute_url(self):
-        return reverse('category-detail',kwargs={'slug':self.slug})   
+        return reverse('category-detail',kwargs={''})   
+
+    
 
     def save(self,*args,**kwargs):
         if self.date_created is None:
            self.date_created=timezone.localtime(timezone.now())
         if self.uniqueId is None:
             self.uniqueId=str(uuid4()).split('-')[4]
-            self.slug=slugify('{}{}'.format(self.title, self.uniqueId))
+           
 
 
-        self.slug=slugify['{}{}'.format(self.title,self.uniqueId)]
+        
         self.last_updated=timezone.localtime(timezone.now())
-        super(Category,self).save(*args, **kwargs)        
+        super(Category,self).save(*args, **kwargs)  
+
 class Image(models.Model):
     name = models.CharField(max_length =30)
     descption =models.TextField(null=True,blank=True)
     category= models.ForeignKey(Category, null=True,blank=True,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = 'images/')
 
+    @classmethod  
+    def search_by_category(cls,search_term):
+        category = cls.objects.filter(title__icontains=search_term)
+        return category 
 
 class Location(models.Model):
     name = models.CharField(max_length =30)
